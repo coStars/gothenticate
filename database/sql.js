efa821e4d4bd1b63b41d7ebc3296551e3d6ebb74
4daliaomry4 <alaakhattab92@gmail.com>
2017-03-21 13:59:17 +0200

    create functions for insert , select, update from database, test all those function

--- database/sql.js
+++ database/sql.js
@@ -1,7 +1,13 @@
-const users = "CREATE TABLE IF NOT EXISTS users (id serial ,username text,email text, password text, bio text)";
-const sessions = "CREATE TABLE IF NOT EXISTS session (id uuid ,value text)";
+const users = "CREATE TABLE IF NOT EXISTS users (id serial ,username text,email text, password text, bio text);";
+const sessions = "CREATE TABLE IF NOT EXISTS session (id uuid ,value text);";
+const addUser = "INSERT INTO users (username,email, password) VALUES ('alaa','alaa@alaa.com', '123654');"
+const getUser = "SELECT * FROM users;";
+const updateUser = "UPDATE  users SET username = 'newalaaa' WHERE username = 'alaa' ;"
 
 module.exports = {
   users : users,
-  sessions : sessions
+  sessions : sessions,
+  addUser : addUser,
+  getUser : getUser,
+  updateUser : updateUser
 }
