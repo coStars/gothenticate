d42bab2b37aa70214337e0fe85ae40ef7fa01156
Mohammed Naji <moha.buy@gmail.com>
2017-03-21 14:17:33 +0200

    adding the file to the branch

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const hapi = require('hapi');
+
 const server = new hapi.Server();
-require ('env2')('env');
+require ('env2')('.env');
 server.connection({
-  port:proccess.env.PORT
+  port:process.env.PORT||3000
+});
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
 });
