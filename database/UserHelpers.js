853df9008595c17e2eed8015bef7c253cb2fb1d9
Mohammed Naji <moha.buy@gmail.com>
2017-03-22 11:15:33 +0200

    conflict

--- database/UserHelpers.js
+++ database/UserHelpers.js
@@ -9,6 +9,9 @@ function createUser(client,data, cb) {
     dbUtils.insert(client, 'users', data, cb);
 }
 
+function getUserByPassword(client,email,password, cb) {
+    dbUtils.select(client, 'users', `email='${email}' AND password='${password}' `, cb)
+}
 function updateUserInfo(client,data, cb) {
 
 dbUtils.update(client,'users', data, 'id=', cb);
